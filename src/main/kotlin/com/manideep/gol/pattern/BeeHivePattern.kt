a0bc8cecb65f69f4e96d798a5516025bf5b91ea2
Manigunda <manideep.gunda@studenti.unicam.it>
2021-02-21 15:43:10 +0100

    Game of life

--- src/main/kotlin/com/manideep/gol/pattern/BeeHivePattern.kt
+++ src/main/kotlin/com/manideep/gol/pattern/BeeHivePattern.kt
@@ -5,8 +5,17 @@ import com.manideep.gol.CellPosition
 class BeeHivePattern : GamePattern {
     override var cellPositions = arrayOf(
         CellPosition(12, 12), CellPosition(12, 13),
-        CellPosition(13, 11), CellPosition(13, 13),
-        CellPosition(14, 12)
+        CellPosition(13, 11), CellPosition(13, 14),
+        CellPosition(14, 12), CellPosition(14, 13),
+        CellPosition(12, 25), CellPosition(12, 26),
+        CellPosition(13, 24), CellPosition(13, 27),
+        CellPosition(14, 25), CellPosition(14, 26),
+        CellPosition(18, 12), CellPosition(18, 13),
+        CellPosition(19, 11), CellPosition(19, 14),
+        CellPosition(20, 12), CellPosition(20, 13),
+        CellPosition(18, 25), CellPosition(18, 26),
+        CellPosition(19, 24), CellPosition(19, 27),
+        CellPosition(20, 25), CellPosition(20, 26),
 
     )
     override val patternName: String
