5a3a4df9ffae71437664eb2affb1eaf62fb649ca
Manigunda <manideep.gunda@studenti.unicam.it>
2021-02-21 14:44:21 +0100

    Game of life

--- src/main/kotlin/com/manideep/gol/pattern/BlockPattern.kt
+++ src/main/kotlin/com/manideep/gol/pattern/BlockPattern.kt
@@ -5,7 +5,14 @@ import com.manideep.gol.CellPosition
 class BlockPattern : GamePattern {
     override var cellPositions = arrayOf(
         CellPosition(12, 12), CellPosition(12, 13),
-        CellPosition(13, 12), CellPosition(13, 13)
+        CellPosition(13, 12), CellPosition(13, 13),
+        CellPosition(8, 12), CellPosition(8, 13),
+        CellPosition(9, 12),CellPosition(9, 13),
+        CellPosition(12, 25),CellPosition(12, 26),
+        CellPosition(13, 25),CellPosition(13, 26),
+        CellPosition(8, 25),CellPosition(8, 26),
+        CellPosition(9, 25),CellPosition(9, 26x)
+
 
     )
     override val patternName: String
