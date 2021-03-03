e05002d4047a57019f0c436dae9df02174798d8c
Manideep Gunda <manideep.gunda@studenti.unicam.it>
2021-03-03 15:52:05 +0100

    Update and rename canCellSurviveOrPopulate.kt to CanCellSurviveOrPopulate.kt

--- src/main/kotlin/com/manideep/gol/rules/CanCellSurviveOrPopulate.kt
+++ src/main/kotlin/com/manideep/gol/rules/CanCellSurviveOrPopulate.kt
@@ -4,7 +4,7 @@ package com.manideep.gol.rules
 //    Each cell with two or three neighbors survives - 3rd Check
 //    Each cell with three neighbors becomes populated. - 4th check
 
-     fun canCellSurviveOrPopulate(isAlive: Boolean, countOfNeighborsAlive: Int): Boolean =
+     fun CanCellSurviveOrPopulate(isAlive: Boolean, countOfNeighborsAlive: Int): Boolean =
         if (isAlive && countOfNeighborsAlive in 0..1) false
         else if (isAlive && countOfNeighborsAlive in 4..8) false
         else if (isAlive && countOfNeighborsAlive in 2..3) true
